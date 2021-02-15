a8a3a17d1627b80ba7b1949cc069ea4b6f61e85c
Simon Roth <sr@cyon.ch>
2021-02-15 16:46:49 +0100

    Deployment updated

--- src/.vuepress/dist/deploy.dist.php
+++ src/.vuepress/dist/deploy.dist.php
@@ -1,14 +1,14 @@
-<?php
+<pre><?php
 
 $secret = 'secret';
 
 ob_start();
-echo('<pre>');
 
 echo('Git pull...' . PHP_EOL);
 ob_flush();
 echo exec('git pull');
 ob_flush();
 
-echo('</pre>')
 ob_end_flush();
+
+?></pre>
