2f3a48ee5017d165c904eab46f1b329e78e926a0
Simon Roth <sr@cyon.ch>
2021-02-15 17:29:11 +0100

    Deployment updated

--- src/.vuepress/dist/deploy.dist.php
+++ src/.vuepress/dist/deploy.dist.php
@@ -4,5 +4,10 @@ header("Content-Type: text/plain");
 
 $secret = 'secret';
 
-echo('Git pull...' . PHP_EOL);
-echo exec('git pull');
+exec('git pull', $output);
+
+exec('npm install', $output);
+
+exec('npm run build', $output);
+
+var_dump($output);
